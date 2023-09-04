39c0e0532a0146f2e596616ee2c13ac661e51c59
sarthakagarwal882 <infosarthakagarwal882@gmail.com>
2023-09-05 00:00:10 +0530

    trial-4

--- src/Routes/SearchRoute.jsx
+++ src/Routes/SearchRoute.jsx
@@ -1,11 +1,11 @@
-import SearchResults from '../Components/searchResults/SearchResults'
+import SearchResults from '../Components/searchResults/searchResults'
 import Navbar from '../Components/navbar/Navbar'
 import { useSearchParams } from "react-router-dom"
 
 const SearchRoute=()=>{
     const [searchParam, setSearchParam] = useSearchParams()
     let query = searchParam.get('query')
-    
+
     return(
         <div>
         <Navbar/>
